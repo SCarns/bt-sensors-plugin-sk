84094e328ace0cff23650a4401fb6f91021f48f7
SCarns <39605258+SCarns@users.noreply.github.com>
2024-09-30 18:53:33 -0400

    Update and rename RUUVI to RUUVI.js

---
+++ sensor_classes/RUUVI.js
@@ -0,0 +1,30 @@
+const BTSensor = require("../BTSensor");
+const LYWSD03MMC = require('./LYWSD03MMC.js')
+class RUUVI extends BTSensor{
+
+    constructor(device){
+        super(device)
+    }
+
+    static metadata = LYWSD03MMC.metadata
+    
+    connect() {
+        const cb = async (propertiesChanged) => {
+            try{
+                this.device.getServiceData().then((data)=>{             
+                    //TBD Check if the service ID is universal across ATC variants
+                    const buff=data['0000181a-0000-1000-8000-00805f9b34fb'];
+                    this.emit("temp",((buff.readInt16LE(6))/100) + 273.1);
+                    this.emit("humidity",buff.readUInt16LE(8)/10000);
+                    this.emit("voltage",buff.readUInt16LE(10)/1000);
+                })
+            }
+            catch (error) {
+                throw new Error(`Unable to read data from ${util.inspect(device)}: ${error}` )
+            }
+        }
+        cb();
+        this.device.helper.on('PropertiesChanged', cb)
+    }
+}
+module.exports=ATC
