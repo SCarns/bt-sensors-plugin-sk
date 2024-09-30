4de62b375c3800bc17bd4326242fe6a420f5361d
SCarns <39605258+SCarns@users.noreply.github.com>
2024-09-30 19:22:49 -0400

    Update RUUVI.js

--- sensor_classes/RUUVI.js
+++ sensor_classes/RUUVI.js
@@ -1,33 +1,49 @@
 const BTSensor = require("../BTSensor");
-const LYWSD03MMC = require('./LYWSD03MMC.js')
-class RUUVI extends BTSensor{
+
+class Ruuvi extends BTSensor{
+
+    static needsScannerOn(){
+        return false
+    }
+    static metadata = new Map()
+                    .set('temp',{unit:'K', description: 'temperature'})
+                    .set('humidity',{unit:'ratio', description: 'humidity'})
+                    .set('pressure',{unit:'Pa', description: 'pressure'})
+                    .set('accel-x',{unit:'', description: 'acceleration-x'})  
+                    .set('accel-y',{unit:'', description: 'acceleration-y'})
+                    .set('accel-z',{unit:'', description: 'acceleration-z'})
+                    .set('rssi',{unit:'', description: 'transmit power'})
+                    .set('voltage',{unit:'V', description: 'sensor battery voltage'})
 
     constructor(device){
         super(device)
     }
 
-    static metadata = LYWSD03MMC.metadata
-    
-    connect() {
-        const cb = async (propertiesChanged) => {
-            try{
-                this.device.getServiceData().then((data)=>{             
-                    //TBD Check if the service ID is universal across ATC variants
-                    const buff=data['0000181a-0000-1000-8000-00805f9b34fb'];
-                    this.emit("temp",((buff.readInt16LE(1))/100) + 273.1);
-                    this.emit("humidity",buff.readUInt16LE(3)/10000);
-                    this.emit("pressure",buff.readUInt16LE(5)/1000);
-                    this.emit("accel-x",buff.readUInt16LE(5));
-                    this.emit("accel-y",buff.readUInt16LE(5));
-                    this.emit("accel-z",buff.readUInt16LE(5));
-                })
-            }
-            catch (error) {
-                throw new Error(`Unable to read data from ${util.inspect(device)}: ${error}` )
-            }
-        }
-        cb();
-        this.device.helper.on('PropertiesChanged', cb)
+    emitValues(buffer){
+        this.emit("temp",((buffer.readInt16LE(10))/100) + 273.1);
+        this.emit("humidity",buffer.readUInt8(10)/100);
+        this.emit("pressure",buffer.readUInt8(0)/100);
+        this.emit("accel-x",buffer.readUInt8(0));
+        this.emit("accel-y",buffer.readUInt8(0));
+        this.emit("accel-z",buffer.readUInt8(0));
+        this.emit("rssi",buffer.readUInt8(0));
+        this.emit("voltage",buffer.readUInt16LE(0)/1000);
+    }
+
+    async connect() {
+        await this.device.connect()
+        var gattServer = await this.device.gatt()
+        var gattService = await gattServer.getPrimaryService("ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6")
+        var gattCharacteristic = await gattService.getCharacteristic("ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6")
+        this.emitValues(await gattCharacteristic.readValue())
+        await gattCharacteristic.startNotifications();	
+        gattCharacteristic.on('valuechanged', buffer => {
+            this.emitValues(buffer)
+        })
+    }
+    async disconnect(){
+        super.disconnect()
+        await this.device.disconnect()
     }
 }
-module.exports=ATC
+module.exports=LYWSD03MMC
