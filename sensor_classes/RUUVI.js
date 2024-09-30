668e3ac702000c42cf00ce1c44d8d988dff7523d
SCarns <39605258+SCarns@users.noreply.github.com>
2024-09-30 18:57:20 -0400

    Update RUUVI.js

--- sensor_classes/RUUVI.js
+++ sensor_classes/RUUVI.js
@@ -14,9 +14,12 @@ class RUUVI extends BTSensor{
                 this.device.getServiceData().then((data)=>{             
                     //TBD Check if the service ID is universal across ATC variants
                     const buff=data['0000181a-0000-1000-8000-00805f9b34fb'];
-                    this.emit("temp",((buff.readInt16LE(6))/100) + 273.1);
-                    this.emit("humidity",buff.readUInt16LE(8)/10000);
-                    this.emit("voltage",buff.readUInt16LE(10)/1000);
+                    this.emit("temp",((buff.readInt16LE(1))/100) + 273.1);
+                    this.emit("humidity",buff.readUInt16LE(3)/10000);
+                    this.emit("pressure",buff.readUInt16LE(5)/1000);
+                    this.emit("accel-x",buff.readUInt16LE(5));
+                    this.emit("accel-y",buff.readUInt16LE(5));
+                    this.emit("accel-z",buff.readUInt16LE(5));
                 })
             }
             catch (error) {
